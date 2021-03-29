b2c18609ed92fa1fadda20db4a4b7ec060a47837
louisreed73 <luis.sa1973@gmail.com>
2021-03-29 18:20:33 +0200

    main routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SearchLayoutComponent } from './layouts/search-layout/search-layout.component';
+import { SearchComponent } from './pages/search/search.component';
+import { DocumentsComponent } from './pages/documents/documents.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'search' },
+  { path: '', component: SearchLayoutComponent, children:
+      [
+        { path: 'search', component: SearchComponent },
+      ]
+ },
+  { path: 'documents', component: DocumentsComponent },
+  { path: '**', redirectTo: "search" }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule],
+  declarations: [
+    SearchLayoutComponent,
+    SearchComponent,
+    DocumentsComponent
+  ]
 })
 export class AppRoutingModule { }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Madrid-Digital';
+  title = 'Madrid-Digital update';
 }
