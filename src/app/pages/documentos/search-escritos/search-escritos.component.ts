e61b36d030d6bacbbd13105c03afe5d928b5f227
louisreed73 <luis.sa1973@gmail.com>
2021-04-06 17:33:03 +0200

    escritos-resoluciones refactor document

--- src/app/pages/documentos/search-escritos/search-escritos.component.ts
+++ src/app/pages/documentos/search-escritos/search-escritos.component.ts
@@ -1,68 +1,80 @@
-import { DOCUMENT } from '@angular/common';
-import { ChangeDetectionStrategy, Component, Inject, OnDestroy} from '@angular/core';
-import { EMPTY, of, Subscription } from 'rxjs';
-import { catchError, tap } from 'rxjs/operators';
-import { DocsEscritosService } from 'src/app/services/docs-escritos.service';
-import { DocumentosService } from 'src/app/services/documentos.service';
-import { SpinnerService } from 'src/app/services/spinner.service';
-import { log } from 'src/app/utilities/utilities';
+import { DOCUMENT } from "@angular/common";
+import {
+     ChangeDetectionStrategy,
+     Component,
+     Inject,
+     OnDestroy,
+} from "@angular/core";
+import { of, Subscription } from "rxjs";
+import { catchError, tap } from "rxjs/operators";
+import { DocsEscritosService } from "src/app/services/docs-escritos.service";
+import { DocumentosService } from "src/app/services/documentos.service";
+import { SpinnerService } from "src/app/services/spinner.service";
 
 @Component({
-  selector: 'app-search-escritos',
-  templateUrl: './search-escritos.component.html',
-  styleUrls: ['./search-escritos.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+     selector: "app-search-escritos",
+     templateUrl: "./search-escritos.component.html",
+     styleUrls: ["./search-escritos.component.scss"],
+     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SearchEscritosComponent implements OnDestroy {
+     /*=============================================
+    =            Observables            =
+    =============================================*/
 
-  // Nos subscribimos al Observable de Documentos acumulados de pagination. Para actualizar en esta sección el filtro global del término de búsqueda.
-  documentosSub:Subscription=this.documentos.documentos$.subscribe();
-  errorObj;
-  // Recibimos el Observable con los datos del número total de documentos/Tipo escritos por término de búsqueda acumulado en pagination.
-  docsEscritos$=this.docsEscritos.docsEscritosSource$
-  .pipe(
-    tap((escritos) => {
-      log(escritos,"El array acumulado es:","lime");
+     // Recibimos el Observable con los datos del número total de documentos / Escritos / por término de búsqueda acumulado en pagination.
+     docsEscritos$ = this.docsEscritos.docsEscritosSource$.pipe(
+          catchError((e: any) => {
+               // Saving error message from http Request Error
+               this.errorObj = e.name;
+               //TODO to comment and uncomment if necessary for checking response and reload page
+               //TODO stopping spinner on http request error
+               // this.spinner.requestSpinner$.next(false);
+               //TODO to remove only for checking response and reload page
+               setTimeout(() => {
+                    this._document.defaultView.location.reload();
+               }, 2000);
+               return of([]);
+          })
+     );
 
-    }),
-    catchError((e: any) => {
-      log(e, "esto es un error?", "red")
-      this.errorObj=e.name;
-      log(this.errorObj, "esto es un error?", "gold")
-      // this.spinner.requestSpinner$.next(false);  
-      
-      setTimeout(() => {
-        this._document.defaultView.location.reload();
-      }, 2000);     
-      return of([])
-    })
-  )
+     // Observable con el nº total de documentos / Escritos / del término de búsqueda
+     docsEscritosLength$ = this.docsEscritos.documentosEscritosLength$;
 
-  docsEscritosLength$=this.docsEscritos.documentosEscritosLength$
+     /*=====  End of Observables  ======*/
 
-  
-  constructor(
-    private documentos : DocumentosService,
-    private docsEscritos: DocsEscritosService,
-    private spinner: SpinnerService,
+     /*=============================================
+ =            Subscriptions            =
+ =============================================*/
 
-    @Inject(DOCUMENT) private _document: Document
+     // Nos subscribimos al Observable de Documentos acumulados de pagination. Para actualizar en esta sección el filtro global del término de búsqueda.
+     documentosSub: Subscription = this.documentos.documentos$.subscribe();
 
-    ) { 
+     /*=====  End of Subscriptions  ======*/
 
+     /*=============================================
+=            Error Obj member            =
+=============================================*/
 
-  }
-  
+     // To Save error message in case Http Request Error
+     errorObj;
 
-  // Método para comprobar que los datos del Observable son efectivamente un array
-  isArray(obj) {
-    return Array.isArray(obj)
-  }
+     /*=====  End of Error Obj member  ======*/
 
-  ngOnDestroy(): void {
-    this.documentosSub.unsubscribe();
-  }
+     constructor(
+          private documentos: DocumentosService,
+          private docsEscritos: DocsEscritosService,
+          private spinner: SpinnerService,
+          //TODO to remove only for checking response and reload page
+          @Inject(DOCUMENT) private _document: Document
+     ) {}
 
+     // Método para comprobar que los datos del OBservable son efectivamente un array
+     isArray(obj) {
+          return Array.isArray(obj);
+     }
 
-
+     ngOnDestroy(): void {
+          this.documentosSub.unsubscribe();
+     }
 }
--- src/app/pages/documentos/search-resoluciones/search-resoluciones.component.ts
+++ src/app/pages/documentos/search-resoluciones/search-resoluciones.component.ts
@@ -1,72 +1,80 @@
-import { DOCUMENT } from '@angular/common';
-import { ChangeDetectionStrategy, Component, Inject, OnDestroy, OnInit } from '@angular/core';
-import { EMPTY, of, Subscription } from 'rxjs';
-import { catchError, tap } from 'rxjs/operators';
-import { DocsResolucionesService } from 'src/app/services/docs-resoluciones.service';
-import { DocumentosService } from 'src/app/services/documentos.service';
-import { SpinnerService } from 'src/app/services/spinner.service';
-import { log } from 'src/app/utilities/utilities';
+import { DOCUMENT } from "@angular/common";
+import {
+     ChangeDetectionStrategy,
+     Component,
+     Inject,
+     OnDestroy,
+} from "@angular/core";
+import { of, Subscription } from "rxjs";
+import { catchError} from "rxjs/operators";
+import { DocsResolucionesService } from "src/app/services/docs-resoluciones.service";
+import { DocumentosService } from "src/app/services/documentos.service";
+import { SpinnerService } from "src/app/services/spinner.service";
 
 @Component({
-  selector: 'app-search-resoluciones',
-  templateUrl: './search-resoluciones.component.html',
-  styleUrls: ['./search-resoluciones.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+     selector: "app-search-resoluciones",
+     templateUrl: "./search-resoluciones.component.html",
+     styleUrls: ["./search-resoluciones.component.scss"],
+     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SearchResolucionesComponent implements OnDestroy {
+     /*=============================================
+    =            Observables            =
+    =============================================*/
 
-  // Nos subscribimos al Observable de Documentos acumulados de pagination. Para actualizar en esta sección el filtro global del término de búsqueda.
-  documentosSub:Subscription=this.documentos.documentos$.subscribe();
-  errorObj;
-  // resoluciones_:any;
-  // cualquiera:any[]=[]
-  // Recibimos el Observable con los datos del número total de documentos/Tipo resoluciones por término de búsqueda acumulado en pagination.
-  docsResoluciones$=this.docsResoluciones.docsResolucionesSource$
-  .pipe(
-    tap((resoluciones) => {
-      log(resoluciones,"El array acumulado es:","lime");
-      // this.resoluciones_=resoluciones;
-    }),
-    catchError((e: any) => {
-      // log(e, "esto es un error?", "red");
-      this.errorObj=e.name;
-      log(this.errorObj, "esto es un error?", "gold")
-      // this.spinner.requestSpinner$.next(false); 
-      setTimeout(() => {
-        this._document.defaultView.location.reload();
-      }, 2000);        
-      return of([])
-    })
-  )
-  docsResolucionesLength$=this.docsResoluciones.documentosResolucionesLength$
-  
+     // Recibimos el Observable con los datos del número total de documentos / Resoluciones / por término de búsqueda acumulado en pagination.
+     docsResoluciones$ = this.docsResoluciones.docsResolucionesSource$.pipe(
+          catchError((e: any) => {
+               // Saving error message from http Request Error
+               this.errorObj = e.name;
+               //TODO to comment and uncomment if necessary for checking response and reload page
+               //TODO stopping spinner on http request error
+               // this.spinner.requestSpinner$.next(false);
+               //TODO to remove only for checking response and reload page
+               setTimeout(() => {
+                    this._document.defaultView.location.reload();
+               }, 2000);
+               return of([]);
+          })
+     );
 
+     // Observable con el nº total de documentos / Resoluciones / del término de búsqueda
+     docsResolucionesLength$ = this.docsResoluciones.documentosResolucionesLength$;
 
+     /*=====  End of Observables  ======*/
 
+     /*=============================================
+ =            Subscriptions            =
+ =============================================*/
 
-  constructor(
-    private documentos : DocumentosService,
-    private docsResoluciones : DocsResolucionesService,
-    private spinner: SpinnerService,
-    @Inject(DOCUMENT) private _document: Document
+     // Nos subscribimos al Observable de Documentos acumulados de pagination. Para actualizar en esta sección el filtro global del término de búsqueda.
+     documentosSub: Subscription = this.documentos.documentos$.subscribe();
 
+     /*=====  End of Subscriptions  ======*/
 
+     /*=============================================
+=            Error Obj member            =
+=============================================*/
 
-    ) {
+     // To Save error message in case Http Request Error
+     errorObj;
 
-  }
+     /*=====  End of Error Obj member  ======*/
 
+     constructor(
+          private documentos: DocumentosService,
+          private docsResoluciones: DocsResolucionesService,
+          private spinner: SpinnerService,
+          //TODO to remove only for checking response and reload page
+          @Inject(DOCUMENT) private _document: Document
+     ) {}
 
+     // Método para comprobar que los datos del OBservable son efectivamente un array
+     isArray(obj) {
+          return Array.isArray(obj);
+     }
 
-  // Método para comprobar que los datos del OBservable son efectivamente un array
-  isArray(obj) {
-    return Array.isArray(obj)
-  }
-
-  ngOnDestroy(): void {
-    //Called once, before the instance is destroyed.
-    //Add 'implements OnDestroy' to the class.
-    this.documentosSub.unsubscribe();
-  }
-
+     ngOnDestroy(): void {
+          this.documentosSub.unsubscribe();
+     }
 }
