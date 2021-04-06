087d4b3c157b2452dec86a3c947f0c3e8426a23c
louisreed73 <luis.sa1973@gmail.com>
2021-04-06 17:05:20 +0200

    search-documents refactor document

--- src/app/pages/documentos/search-documents/search-documents.component.ts
+++ src/app/pages/documentos/search-documents/search-documents.component.ts
@@ -1,64 +1,60 @@
-import { DOCUMENT } from '@angular/common';
-import { ChangeDetectionStrategy, Component, Inject} from '@angular/core';
-import { of} from 'rxjs';
-import { catchError, tap } from 'rxjs/operators';
-import { DocumentosService } from 'src/app/services/documentos.service';
-import { SpinnerService } from 'src/app/services/spinner.service';
-import { log } from 'src/app/utilities/utilities';
-
+import { DOCUMENT } from "@angular/common";
+import { ChangeDetectionStrategy, Component, Inject } from "@angular/core";
+import { of } from "rxjs";
+import { catchError } from "rxjs/operators";
+import { DocumentosService } from "src/app/services/documentos.service";
+import { SpinnerService } from "src/app/services/spinner.service";
 
 @Component({
-  selector: 'app-search-documents',
-  templateUrl: './search-documents.component.html',
-  styleUrls: ['./search-documents.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+     selector: "app-search-documents",
+     templateUrl: "./search-documents.component.html",
+     styleUrls: ["./search-documents.component.scss"],
+     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SearchDocumentsComponent {
+     /*=============================================
+    =            Observables            =
+    =============================================*/
 
-  inicio: boolean = true;
-  errorObj;  
-  // Recibimos el Observable con los datos del número total de documentos por término de búsqueda acumulado en pagination.
-  documentos$ = this.documentos
-    .documentos$
-    .pipe(
-      tap((v) => {
-        log(v,"que es esto:","gold")
-        this.inicio = false;
-      }),
-      catchError((e: any) => {
-        // log(e, "esto es un error?", "red")
-        this.errorObj=e.name;
-        log(this.errorObj, "error en search-documents?", "gold")
-        // this.spinner.requestSpinner$.next(false); 
-        setTimeout(() => {
-          this._document.defaultView.location.reload();
-        }, 2000);     
-        return of([])
-      })
-    )
-    docsLength$=this.documentos.documentosLength$
-    
+     // Recibimos el Observable con los datos del número total de documentos por término de búsqueda acumulado en pagination.
+     documentos$ = this.documentos.documentos$.pipe(
+          catchError((e: any) => {
+               // Saving error message from http Request Error
+               this.errorObj = e.name;
+               //TODO to comment and uncomment if necessary for checking response and reload page
+               //TODO stopping spinner on http request error
+               // this.spinner.requestSpinner$.next(false);
+               //TODO to remove only for checking response and reload page
+               setTimeout(() => {
+                    this._document.defaultView.location.reload();
+               }, 2000);
+               return of([]);
+          })
+     );
 
+     // Observable con el nº total de documentos del término de búsqueda
+     docsLength$ = this.documentos.documentosLength$;
 
-  constructor(
-    private documentos: DocumentosService,
-    private spinner: SpinnerService,
-    @Inject(DOCUMENT) private _document: Document
+     /*=====  End of Observables  ======*/
 
+     /*=============================================
+    =            Error Obj member            =
+    =============================================*/
 
-    
-  ) {
+     // To Save error message in case Http Request Error
+     errorObj;
 
+     /*=====  End of Error Obj member  ======*/
 
+     constructor(
+          private documentos: DocumentosService,
+          private spinner: SpinnerService,
+          //TODO to remove only for checking response and reload page
+          @Inject(DOCUMENT) private _document: Document
+     ) {}
 
-  }
-
-
-  // Método para comprobar que los datos del OBservable son efectivamente un array
-  isArray(obj) {
-    return Array.isArray(obj)
-  }
-
-
-
+     // Método para comprobar que los datos del OBservable son efectivamente un array
+     isArray(obj) {
+          return Array.isArray(obj);
+     }
 }
