0275ffd43d45db2a8b723d28de7effb94aa66b18
louisreed73 <luis.sa1973@gmail.com>
2021-04-21 10:15:18 +0200

    eliminacion servicio documentos y componente filtros, ahora es filtro component

--- src/app/sharedModules/shared-utilities/shared-utilities.module.ts
+++ src/app/sharedModules/shared-utilities/shared-utilities.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { SpinnerComponent } from "../../sharedComponents/spinner/spinner.component";
 import { MatProgressSpinnerModule } from "@angular/material";
-import { FiltrosComponent } from "../../sharedComponents/filtros/filtros.component";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { FijarDirective } from "src/app/directives/fijar.directive";
 import { FiltroComponent } from '../../sharedComponents/filtro/filtro.component';
@@ -11,7 +10,6 @@ import { AutocompleteComponent } from '../../sharedComponents/autocomplete/autoc
 @NgModule({
      declarations: [
           SpinnerComponent,
-          FiltrosComponent,
           FijarDirective,
           AutocompleteComponent,
           FiltroComponent,
@@ -28,7 +26,6 @@ import { AutocompleteComponent } from '../../sharedComponents/autocomplete/autoc
           ReactiveFormsModule,
           SpinnerComponent, 
           MatProgressSpinnerModule, 
-          FiltrosComponent,
           FiltroComponent,
           FijarDirective,
           AutocompleteComponent
