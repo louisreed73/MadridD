09aaea2fe3238ea33738b18a810fdf22c3d13760
louisreed73 <luis.sa1973@gmail.com>
2021-04-29 14:55:22 +0200

    to clean a lot of things, the only trigger is buscar button

---
+++ src/app/services/search-trigger.service.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SearchTriggerService {
+
+  newTriggerSearch:Subject<any>= new Subject<any>();
+  triggerSearch:Observable<any>=this.newTriggerSearch.asObservable();
+
+  _updatedFiltro:{[k:string]:any}={};
+  _updatedSearch:string=null;
+  _updatedPagina:number=null;
+
+
+  set updatedFiltro(v:{[k:string]:any}) {
+    console.log("Me acaban de actualizar!!!");
+    this._updatedFiltro=v;
+    console.log(this.updatedFiltro);
+  }
+
+  get updatedFiltro() {
+    return this._updatedFiltro
+  }
+  
+  set updatedSearch(v:string) {
+    console.log("Me acaban de actualizar!!!");
+    this._updatedSearch=v;
+    console.log(this.updatedSearch);
+  }
+
+  get updatedPagina() {
+    return this._updatedPagina
+  }
+  
+  set updatedPagina(v:number) {
+    console.log("Me acaban de actualizar!!!");
+    this._updatedPagina=v;
+    console.log(this.updatedPagina);
+  }
+
+  get updatedSearch() {
+    return this._updatedSearch
+  }
+  
+
+  constructor() { 
+  }
+}
---
+++ src/app/sharedComponents/button-trigger/button-trigger.component.ts
@@ -0,0 +1,60 @@
+import {
+     AfterViewInit,
+     Component,
+     ElementRef,
+     Inject,
+     OnInit,
+     ViewChild,
+} from "@angular/core";
+import { fromEvent, merge } from "rxjs";
+import { tap } from "rxjs/operators";
+import { SearchTriggerService } from "src/app/services/search-trigger.service";
+
+@Component({
+     selector: "app-button-trigger",
+     templateUrl: "./button-trigger.component.html",
+     styleUrls: ["./button-trigger.component.scss"],
+})
+export class ButtonTriggerComponent implements OnInit, AfterViewInit {
+     @ViewChild("searchTrigger", { static: true }) button: ElementRef;
+
+     constructor(
+       @Inject(Window) private window: Window,
+       private searchTrigger:SearchTriggerService
+       ) {}
+
+     ngOnInit() {}
+
+     ngAfterViewInit(): void {
+          //Called after ngAfterContentInit when the component's view has been initialized. Applies to components only.
+          //Add 'implements AfterViewInit' to the class.
+          merge(
+               fromEvent(window.document, "keyup"),
+               fromEvent(this.button.nativeElement, "click")
+          )
+               .pipe(
+                //  tap(console.log)
+               )
+               .subscribe((event: Event) => {
+                    // console.log(event);
+                    if (
+                         event instanceof KeyboardEvent &&
+                         event.key === "Enter"
+                    ) {
+                        //  console.log(event.key);
+                        
+                        this.window.scrollTo(0, 0);
+                        this.searchTrigger.updatedPagina=1;
+                        this.searchTrigger.newTriggerSearch.next("busca")
+                    }
+                    
+                    if (event instanceof MouseEvent) {
+                         
+                         // console.log(event.type)
+                         this.window.scrollTo(0, 0);
+                          this.searchTrigger.updatedPagina=1;
+                          this.searchTrigger.newTriggerSearch.next("busca")
+                    }
+               });
+     }
+}
