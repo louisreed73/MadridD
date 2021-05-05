96ba2d1c28924322e2bbeea5ed8a97a6d718931b
louisreed73 <luis.sa1973@gmail.com>
2021-05-05 09:33:23 +0200

    collapsable toggle for each filter, collapsable new shared component

---
+++ src/app/sharedComponents/filtro-toggle/filtro-toggle.component.ts
@@ -0,0 +1,21 @@
+import { Component, ContentChild, ElementRef, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-filtro-toggle',
+  templateUrl: './filtro-toggle.component.html',
+  styleUrls: ['./filtro-toggle.component.scss']
+})
+export class FiltroToggleComponent implements OnInit {
+
+  // @Input() toggle:any; 
+
+  @ContentChild("toggleEl", {static:true}) input:ElementRef
+  
+  constructor() { }
+
+  ngOnInit() {
+
+    // console.log(this.input)
+  }
+
+}
